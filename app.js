753df5fca4ed8f37f85c018017bb785440987059
unknown <nipdev@yahoo.com>
2025-01-19 19:58:27 -0300

    Atualizando...

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
